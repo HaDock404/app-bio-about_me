dd41409ce9997e2f7a26507a40876ec227904ff8
HaDock404 <delescluse.g@gmail.com>
2024-07-24 08:31:13 +0200

    background video

---
+++ src/components/Video.jsx
@@ -0,0 +1,12 @@
+import '../styles/video.css'
+import Video_mp4 from '../assets/video.mp4'
+
+function Video() {
+    return (
+        <div className="video">
+            <video src={Video_mp4} autoPlay loop muted></video>
+        </div>
+    )
+}
+
+export default Video
--- src/pages/Redirection.jsx
+++ src/pages/Redirection.jsx
@@ -1,10 +1,12 @@
 import Redirection_body from "../components/Redirection_body"
 import Header from "../components/Header"
 import '../styles/redirection.css'
+import Video from "../components/Video"
 
 function Redirection() {
     return (
         <>
+            <Video />
             <Header />
             <Redirection_body/>
         </>
