61bf222a5bae7959eb5bc13a5a4eaea791019f22
HaDock404 <delescluse.g@gmail.com>
2024-08-08 10:33:26 +0200

    All responsive

--- src/components/text_FR/Content3.jsx
+++ src/components/text_FR/Content3.jsx
@@ -9,7 +9,7 @@ function Content3() {
                     <li className="content3__container__list__item" id="opacity">Puis, j’ai utilisé mes compétences managériales dans la rénovation d’un immeuble Parisien de 6 étages</li>
                     <li className="content3__container__list__item" id="opacity">.. (sans ascenseur)</li>
                     <li className="content3__container__list__item" id="opacity">J’avais la charge complète du management des travaux ..</li>
-                    <li className="content3__container__list__item" id="content3_4">Organisation des équipes, sécurité, commande de matériaux, réunions client et assurer la qualité des ouvrages</li>
+                    <li className="content3__container__list__item testX" id="content3_4">Organisation des équipes, sécurité, commande de matériaux, réunions client et assurer la qualité des ouvrages</li>
                   </ul>
                 </div>
             </div>
